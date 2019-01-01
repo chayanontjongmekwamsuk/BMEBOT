f0b4b5876255003a0df0c9deb930cef17e28b632
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-01 23:19:16 +0700

    Say welcome to Nodejs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* initial return hello
+// initial return hello
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
@@ -6,12 +6,14 @@ var port = process.env.PORT || 3000;
 app.get("/", function(req, res){
     res.send("welcome to NodeJs");
 });
-*/
 
+
+/*
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.post('/webhook', (req, res) => res.sendStatus(200));
+*/
 
 app.listen(port);
