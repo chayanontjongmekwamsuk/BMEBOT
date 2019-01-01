976d6c7b47bcf65b50af2847e927dbe79b4f284f
Chayanont Jongmekwamsuk <45491604+chayanontjongmekwamsuk@users.noreply.github.com>
2019-01-01 23:13:15 +0700

    setup

---
+++ index.js
@@ -0,0 +1,17 @@
+/* initial return hello
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get("/", function(req, res){
+    res.send("welcome to NodeJs");
+});
+*/
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.post('/webhook', (req, res) => res.sendStatus(200));
+
+app.listen(port);
