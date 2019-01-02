6bf11672c3763d73d3d238f9d6523b888175e273
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-02 23:15:29 +0700

    play

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
 
     let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
-    reply(reply_token, msg)  
+    //let msg = req.body.events[0].message.text
+    reply(reply_token)  
+    //reply(reply_token, msg)  
     res.sendStatus(200)
 })
 
 app.listen(port)
 
-function reply(reply_token, msg) {
+//function reply(reply_token, msg) {
+function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {leSLT6TM73BCkORSMedsDEI0MfrS29lfV6wIIsXbF6UmJ5Y2d+Y80RAlxtIkfFuPhJOL5+8lx4Wyv6ojj1Eibr04O1n3fORRGHwUnIyM8tfV//liRGkp73cDYiCTN/ZTkd42KICBYRCWO4ctm02u/wdB04t89/1O/w1cDnyilFU=}'
@@ -27,20 +29,22 @@ function reply(reply_token, msg) {
 
     let body = JSON.stringify({
         replyToken: reply_token,
-        /*
+        
         messages: [{
             type: 'text',
-            text: 'Hello I am sunny'
+            text: 'Hi Aoff who is the most beautiful girl'
         },
         {
             type: 'text',
             text: 'How can I help you?'
-        }]*/
+        }]
 
+        /*
         messages: [{
             type: 'text',
             text: msg
         }]
+        */
     })
 
     request.post({
