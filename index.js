a141cd8909ef7b5169ef23262ba3da26e4fab56c
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-02 21:04:14 +0700

    start conver

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 // initial return hello
-/*
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.get("/", function(req, res){
-    res.send("welcome to NodeJs");
-});
-*/
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
-
-app.post('/webhook', (req, res) => {
-     res.sendStatus(200);
-});
 
 app.listen(port);
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {xxxxxxx}'
+    }
+
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+
+        console.log('status = ' + res.statusCode);
+    });
+}
