bdd61c40ea5a7241cfd5d24fe579eb7a087d7ef2
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-06 22:03:59 +0700

    add @line-nodejs_change-event-to-1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
 
-    let reply_token = req.body.events[0].replyToken
+    let reply_token = req.body.events[1].replyToken
     let msg = req.body.events[0].message.text
     //reply(reply_token)  
     reply(reply_token, msg)  
