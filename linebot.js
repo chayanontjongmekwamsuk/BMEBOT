9a701a2dd9a45d7e48e027c45056e0cac14c2350
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-08 23:20:16 +0700

    push_reply

--- linebot.js
+++ linebot.js
@@ -4,41 +4,58 @@ const request = require('request')
 
 const app = express()
 const port = process.env.PORT || 4000   // whatever is in the environment variable PORT or 3000 if there's nothing there.
+const Headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {leSLT6TM73BCkORSMedsDEI0MfrS29lfV6wIIsXbF6UmJ5Y2d+Y80RAlxtIkfFuPhJOL5+8lx4Wyv6ojj1Eibr04O1n3fORRGHwUnIyM8tfV//liRGkp73cDYiCTN/ZTkd42KICBYRCWO4ctm02u/wdB04t89/1O/w1cDnyilFU=}'
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// pushing
+app.get('/webhook', (req, res) => {
+    let smsg = 'Welcome, Can I help you'
+    push(smsg)
+    res.send(smsg)
+})
+
+// replying
 app.post('/webhook', (req, res) => {
 
     let reply_token = req.body.events[0].replyToken     // cannot change event
     let msg = req.body.events[0].message.text
-    //3/ push(reply_token,msg)    // didn't work
-    //1/ reply(reply_token)  
-
-    reply(reply_token, msg)     // send to function
+    reply(reply_token,msg)    
     res.sendStatus(200)
 })
 
 app.listen(port)    // makes your server be able to accept a parameter from the environment that port to listen on, depending on your requirement and the requirement of the environment
 
+function push(smsg){
+    let body = JSON.stringify({
+        to: 'U2cce7962a9ba284308540b3b28ffa885',
+    messages: [{
+        type: 'text',
+        text: smsg
+    }]   
+    
+  })
+  curl('push', body)
+}
+
 
 function reply(reply_token, msg) {
-//1/function reply(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {leSLT6TM73BCkORSMedsDEI0MfrS29lfV6wIIsXbF6UmJ5Y2d+Y80RAlxtIkfFuPhJOL5+8lx4Wyv6ojj1Eibr04O1n3fORRGHwUnIyM8tfV//liRGkp73cDYiCTN/ZTkd42KICBYRCWO4ctm02u/wdB04t89/1O/w1cDnyilFU=}'
-    }
-    
+        
     // if("msg" == "hi"){
     let body = JSON.stringify({
         replyToken: reply_token,
         
         messages: [{                // answer normally
                  type: 'text',
-                 text: 'Hi'
+                 text: 'Your word'
             },
             {
                 type: 'text',
-                text: 'How can I help you?'
+                text: msg
             }]
         
    
@@ -121,13 +138,16 @@ function reply(reply_token, msg) {
           
 
     })
+    curl('reply', body)
 }
 
+function curl(method, body){
     request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
+        url: 'https://api.line.me/v2/bot/message/' + method,
+        headers: Headers,
         body: body
     }, (err, res, body) => {
 
         console.log('status = ' + res.statusCode);
     });
+}
